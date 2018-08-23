bc9c42a5263b28f647a9feaa0567c46dc7bcde9e
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-08-23 18:05:09 +0200

    [sa] coded

---
+++ includes/push_swap.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrossouw <hrossouw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/23 13:32:28 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/08/23 13:48:11 by hrossouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _PUSH_SWAP_H
+# define _PUSH_SWAP_H
+#include "../libft/includes/libft.h"
+
+typedef struct		s_node
+{
+	int				data;
+	struct s_node	*next;
+	struct s_node	*prev;
+}					t_node;
+
+typedef struct		s_stack
+{
+	t_node			*a_head;
+	t_node			*b_head;
+	t_node			*a_tail;
+	t_node			*b_tail;
+}					t_stack;
+
+void				op_sa(t_stack *stack);
+void				op_sb(t_stack *stack);
+void				op_ss(t_stack *stack);
+void				op_pa(t_stack *stack);
+void				op_pb(t_stack *stack);
+void				op_ra(t_stack *stack);
+void				op_rb(t_stack *stack);
+void				op_rr(t_stack *stack);
+void				op_rra(t_stack *stack);
+void				op_rrb(t_stack *stack);
+void				op_rrr(t_stack *stack);
+# endif 
---
+++ src/swap_ops.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_ops.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrossouw <hrossouw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/23 13:25:04 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/08/23 18:02:37 by hrossouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	op_sa(t_stack *stack)
+{
+	t_node	*temp;
+
+	if (stack->a_head && stack->a_head->next)
+	{
+		temp = stack->a_head->next;
+		if (stack->a_head->next->next)
+			stack->a_head->next->next->prev = stack->a_head;
+		else
+			stack->a_head->next->next->prev = 0;
+		if (!(stack->a_head->next->next))
+			stack->a_tail = stack->a_head;
+		else
+			stack->a_tail = 0;
+		stack->a_head->next = stack->a_head->next->next;
+		stack->a_head->prev = temp;
+		temp->prev = NULL;
+		temp->next = stack->a_head;
+		stack->a_head = temp;
+		//TODO : add to stack total
+		//TODO : print out
+	}
+}
