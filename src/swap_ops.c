60cfa9f67ea9b121137a8ddf411f80e24459dd29
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-08-28 12:09:09 +0200

    [op_ss] added

--- src/swap_ops.c
+++ src/swap_ops.c
@@ -6,7 +6,7 @@
 /*   By: hrossouw <hrossouw@student.wethinkcode.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/23 13:25:04 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/08/28 11:56:14 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/08/28 12:07:53 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,47 @@ void	op_sb(t_stack *stack)
 		stack->b_head = temp;
 		printf("sb"); //TEST
 	}
+}
+
+void	op_ss(t_stack *stack)
+{
+	t_node	*temp;
+	
+	if (stack->a_head && stack->a_head->next)
+	{
+		temp = stack->a_head->next;
+		if (stack->a_head->next->next)
+			stack->a_head->next->next->prev = stack->a_head;
+		else
+			stack->a_head->next->next->prev = 0;
+		if (!(stack->a_head->next->next))
+			stack->a_tail = stack->a_head;
+		else
+			stack->a_tail = 0;
+		stack->a_head->next = stack->a_head->next->next;
+		stack->a_head->prev = temp;
+		temp->prev = NULL;
+		temp->next = stack->a_head;
+		stack->a_head = temp;
+	}
+
+	temp = stack->b_head->next;
+	if (stack->b_head && stack->b_head->next)
+	{
+		temp = stack->b_head->next;
+		if (stack->b_head->next->next)
+			stack->b_head->next->next->prev = stack->b_head;
+		else
+			stack->b_head->next->next->prev = 0;
+		if (!(stack->b_head->next->next))
+			stack->b_tail = stack->b_head;
+		else
+			stack->b_tail = 0;
+		stack->b_head->next = stack->b_head->next->next;
+		stack->b_head->prev = temp;
+		temp->prev = NULL;
+		temp->next = stack->b_head;
+		stack->b_head = temp;
+	}
+	printf("ss");
 }
