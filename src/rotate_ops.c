c98bafdbaf0fc63920a2bf055a1ac83fa4099c64
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-08-29 12:34:26 +0200

    [op_rr] created

--- src/rotate_ops.c
+++ src/rotate_ops.c
@@ -6,7 +6,7 @@
 /*   By: hrossouw <hrossouw@student.wethinkcode.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 12:12:50 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/08/29 12:26:21 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/08/29 12:30:38 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,31 @@ void	op_rb(t_stack *stack)
 		//increase size of stack total
 		printf("rb");
 	}
+}
+
+void	op_rr(t_stack *stack)
+{
+	t_node *temp;
+	if (stack->a_head && stack->a_head->next)
+	{
+		temp = stack->a_head;
+		stack->a_head = stack->a_head->next;
+		stack->a_head->prev = NULL;
+		temp->prev = stack->a_tail;
+		temp->next = NULL;
+		stack->a_tail->next = temp;
+		stack->a_tail = temp;
+	}
+	if (stack->b_head && stack->b_head->next)
+	{
+		temp = stack->b_head;
+		stack->b_head = stack->b_head->next;
+		stack->b_head->prev = NULL;
+		temp->prev = stack->b_tail;
+		temp->next = NULL;
+		stack->b_tail->next = temp;
+		stack->b_tail = temp;
+	}
+	//increase size of stack total
+	printf("rr");
 }
