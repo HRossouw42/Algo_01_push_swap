ae745688df7b8c87b0525d059206eaeb78480c9f
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-08-29 17:36:20 +0200

    TODO: read input into linked list

---
+++ src/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrossouw <hrossouw@student.wethinkcode.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/29 15:02:13 by hrossouw          #+#    #+#             */
+/*   Updated: 2018/08/29 15:41:51 by hrossouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int		main(void)
+{
+	t_stack stack;
+
+	stack.a_head = NULL;
+	stack.b_head = NULL;
+	stack.a_tail = NULL;
+	stack.b_tail = NULL;
+}
