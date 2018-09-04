e0bb46d880bd171d101f827fad0246044dc23323
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-09-04 11:06:50 +0200

    [lst_print] incomplete, TODO remove head/tail from printing

--- src/main.c
+++ src/main.c
@@ -6,36 +6,27 @@
 /*   By: hrossouw <hrossouw@student.wethinkcode.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 15:02:13 by hrossouw          #+#    #+#             */
-/*   Updated: 2018/09/03 17:23:36 by hrossouw         ###   ########.fr       */
+/*   Updated: 2018/09/04 11:05:56 by hrossouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-t_list		*new_list(void)
-{
-	t_list *new;
-
-	new = (t_list *)malloc(sizeof(t_list));
-	if (new != NULL)
-	{
-		new->len = 0;
-		new->head = NULL;
-		new->tail = NULL;
-	}
-	return (new);
-}
-
 void	lst_print(t_list *lst)
 {
 	t_node *temp;
 
-	temp = lst->data;
-	while(temp)
+	if (lst != NULL)
 	{
-		ft_putnbr(temp->data);
-		temp= temp->next;
+		temp = lst->head;
+		while(temp)
+		{
+			ft_putnbr(temp->data);
+			ft_putstr(" - ");
+			temp = temp ->next;
+		}
 	}
+	ft_putchar('\n');
 }
 
 int		main(int argc, char **argv)
@@ -50,6 +41,6 @@ int		main(int argc, char **argv)
 		la = ft_lst_append(la, ft_atoi(*argv));
 		argv++;
 	}
-	//lst_print(la);
+	lst_print(la);
 	return (0);
 }
